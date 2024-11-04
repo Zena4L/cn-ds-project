bba41670009cc70bb7b76f2e44326f9af4404ae7
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-11-04 15:29:33 +0000

    github:actions

--- product/src/main/kotlin/org/cn/product/utils/ConstantMessage.kt
+++ product/src/main/kotlin/org/cn/product/utils/ConstantMessage.kt
@@ -1,6 +1,5 @@
 package org.cn.product.utils
 
-
 object ErrorMessageConstant {
     const val NOT_FOUND_MESSAGE = "No such product"
     const val INTERNAL_SERVER_ERROR_MSG = "Something went Wrong"
