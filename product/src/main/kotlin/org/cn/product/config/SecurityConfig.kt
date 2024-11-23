14596c9e7876c829d387a1c53d00520917ea53c5
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-11-23 13:19:58 +0000

    authorization: product service

---
+++ product/src/main/kotlin/org/cn/product/config/SecurityConfig.kt
@@ -0,0 +1,41 @@
+package org.cn.product.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { auth ->
+            auth.requestMatchers(HttpMethod.GET, "/", "/api/v1/product/**").permitAll()
+            auth.anyRequest().hasRole("employee")
+        }
+        http.oauth2ResourceServer {
+            it.jwt { }
+        }
+        http.sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+        http.csrf { it.disable() }
+        return http.build()
+    }
+
+    @Bean
+    fun jwtAuthenticationConverter(): JwtAuthenticationConverter {
+        val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_")
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles")
+
+        val jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter)
+
+        return jwtAuthenticationConverter
+    }
+
+}
