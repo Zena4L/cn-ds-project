bffd1e04fbedb929e1fa904aa2a7cdeda64d09d1
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-12-03 08:25:25 +0000

    feat:hateos for product

--- product/src/main/kotlin/org/cn/product/web/ProductController.kt
+++ product/src/main/kotlin/org/cn/product/web/ProductController.kt
@@ -7,18 +7,12 @@ import org.cn.product.dtos.ProductRequest
 import org.cn.product.dtos.ProductUpdated
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.data.domain.Page
+import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/product")
@@ -38,17 +32,30 @@ class ProductController(private val productService: ProductService) {
     }
 
     @GetMapping
-    fun getAllProduct(
-        @RequestParam(defaultValue = "0") page: Int,
-        @RequestParam(defaultValue = "10") size: Int
-    ): Page<Product> {
-        logger.info("Fetching all product")
-        return productService.getAllProducts(page, size)
-
+fun getAllProduct(
+    @RequestParam(defaultValue = "0") page: Int,
+    @RequestParam(defaultValue = "10") size: Int
+): CollectionModel<EntityModel<Product>> {
+    logger.info("Fetching all products")
+    val products = productService.getAllProducts(page, size)
+    val productModels = products.map { product ->
+        EntityModel.of(product,
+            linkTo(methodOn(ProductController::class.java).getProduct(product.id!!)).withSelfRel()
+        )
     }
-
+    return CollectionModel.of(productModels,
+        linkTo(methodOn(ProductController::class.java).getAllProduct(page, size)).withSelfRel()
+    )
+}
     @GetMapping("/{id}")
-    fun getProduct(@PathVariable("id") id: Int) = productService.getProduct(id)
+    fun getProduct(@PathVariable("id") id: Int): EntityModel<Product> {
+
+        logger.info("Fetching product with id : $id")
+        return EntityModel.of(productService.getProduct(id),
+            linkTo(methodOn(ProductController::class.java).getProduct(id)).withSelfRel(),
+            linkTo(methodOn(ProductController::class.java).getAllProduct(0, 10)).withRel("products")
+        )
+    }
 
     @PatchMapping("/{id}")
     fun updateProduct(@PathVariable("id") id: Int, @RequestBody productRequest: ProductUpdated) =
